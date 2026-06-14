998b3ae27e6701ccea029c3fdd32efb32d9afdb8
JUICE <165546855+JUSICK@users.noreply.github.com>
2026-06-14 17:04:14 +0200

    Update security_log_filter.sh

--- bin/security_log_filter.sh
+++ bin/security_log_filter.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Blad: Niepoprawna liczba argumentow." >&2
-    echo "Uzycie: $0 <log>" >&2
+    echo "Usage: $0 <log>" >&2
     exit 1
 fi
 
-log_plik="$1"
+log_file="$1"
 
-if [ ! -f "$log_plik" ]; then
-    echo "Blad: Plik logu '$log_plik' nie istnieje." >&2
+if [ ! -f "$log_file" ]; then
+    echo "ERROR: Log file '$log_file' doesn't exist." >&2
     exit 2
 fi
 
-wyjscie="reports/security_log_filter.txt"
+output="reports/security_log_filter.txt"
 
-liczba=$(grep -E "path_traversal|forbidden|login_failed" "$log_plik" | wc -l)
+num=$(grep -E "path_traversal|forbidden|login_failed" "$log_file" | wc -l)
 
-echo "SUSPICIOUS=$liczba" >> "$wyjscie"
+echo "SUSPICIOUS=$num" >> "$output"
